bd00ba1f5dd90de67462322eec672e35d2390a49
Gourav Kashiv <gouravkashiv3@gmail.com>
2026-02-26 14:27:51 +0530

    chore: untrack supabase and markdown files according to .gitignore

--- supabase/migrations/20240222_add_ical_url.sql
+++
@@ -1,5 +0,0 @@
--- Add icalUrl column to rooms table
-ALTER TABLE rooms ADD COLUMN IF NOT EXISTS icalUrl TEXT;
-
--- Add icalUrl column to cabins table
-ALTER TABLE cabins ADD COLUMN IF NOT EXISTS icalUrl TEXT;
